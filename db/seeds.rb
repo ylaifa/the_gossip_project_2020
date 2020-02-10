ec830129c51436ce4020b1d332dd7ceac2828e83
Yousri <laifayousri@gmail.com>
2020-02-10 17:32:21 +0100

    edit seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.destroy_all
+
+10.times do
+    User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.paragraph, age: Faker::Number.between(from: 18, to: 120), email: Faker::Internet.email)
+  end
