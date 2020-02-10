116b44e5a979b704f753535aca6a76afc4ca8ca9
Yousri <laifayousri@gmail.com>
2020-02-10 19:23:22 +0100

    table gossips created

--- db/migrate/20200210172614_create_gossips.rb
+++ db/migrate/20200210172614_create_gossips.rb
@@ -3,6 +3,7 @@ class CreateGossips < ActiveRecord::Migration[5.2]
     create_table :gossips do |t|
       t.string :title
       t.text :content
+      t.belongs_to :user, index: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_170219) do
+ActiveRecord::Schema.define(version: 2020_02_10_172614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2020_02_10_170219) do
     t.string "zip_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "gossips", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_gossips_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
