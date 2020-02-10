ece0b402df58adc7fdea6eb42d92d55e0acd781f
Yousri <laifayousri@gmail.com>
2020-02-11 00:41:02 +0100

    rollback likes table

--- db/migrate/20200210230702_create_likes.rb
+++ db/migrate/20200210230702_create_likes.rb
@@ -1,6 +1,7 @@
 class CreateLikes < ActiveRecord::Migration[5.2]
   def change
     create_table :likes do |t|
+      t.references :user, index: :true
       t.references :likable, polymorphic: true
       t.timestamps
     end
