ce501e6982a169eaa1de702cb3dc39619fe724bb
Yousri <laifayousri@gmail.com>
2020-02-10 22:35:40 +0100

    tables tags and gossip_tags created

--- db/migrate/20200210213112_create_gossip_tags.rb
+++ db/migrate/20200210213112_create_gossip_tags.rb
@@ -1,6 +1,8 @@
 class CreateGossipTags < ActiveRecord::Migration[5.2]
   def change
     create_table :gossip_tags do |t|
+      t.belongs_to :gossip
+      t.belongs_to :tag
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_172614) do
+ActiveRecord::Schema.define(version: 2020_02_10_213112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2020_02_10_172614) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "gossip_tags", force: :cascade do |t|
+    t.bigint "gossip_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["gossip_id"], name: "index_gossip_tags_on_gossip_id"
+    t.index ["tag_id"], name: "index_gossip_tags_on_tag_id"
+  end
+
   create_table "gossips", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -29,6 +38,12 @@ ActiveRecord::Schema.define(version: 2020_02_10_172614) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_gossips_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
