4bc3de826624080d8fb4a25f6cef09d41b6186fc
Yousri <laifayousri@gmail.com>
2020-02-10 23:29:02 +0100

    table private_messages created

--- db/migrate/20200210220116_create_private_messages.rb
+++ db/migrate/20200210220116_create_private_messages.rb
@@ -2,6 +2,8 @@ class CreatePrivateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :private_messages do |t|
       t.text :content
+      t.references :recipient, index: true
+      t.references :sender, index: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_213112) do
+ActiveRecord::Schema.define(version: 2020_02_10_220116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 2020_02_10_213112) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_gossips_on_user_id"
+  end
+
+  create_table "private_messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "recipient_id"
+    t.bigint "sender_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipient_id"], name: "index_private_messages_on_recipient_id"
+    t.index ["sender_id"], name: "index_private_messages_on_sender_id"
   end
 
   create_table "tags", force: :cascade do |t|
