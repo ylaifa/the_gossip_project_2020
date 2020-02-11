ecbdc67783877c6dbf273cad966b95e7797346ba
Yousri <laifayousri@gmail.com>
2020-02-11 16:00:12 +0100

    welcomes controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'welcomes/index'
   get '/team', to: 'static_pages#team', as: 'team'
   get '/contact', to: 'static_pages#contact', as: 'contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
