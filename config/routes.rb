714155ed4eac11f32096f1168a9e76019950cc14
Yousri <laifayousri@gmail.com>
2020-02-11 16:19:35 +0100

    views WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcomes/index'
-  get '/team', to: 'static_pages#team', as: 'team'
-  get '/contact', to: 'static_pages#contact', as: 'contact'
+  get 'welcomes/:first_name', to: 'welcomes#index'
+  get '/team', to: 'static_pages#team'
+  get '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
