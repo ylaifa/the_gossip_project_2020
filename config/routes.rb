1bae6f9d265ce4e1ba28721e180766c1b2c528f3
Yousri <laifayousri@gmail.com>
2020-02-11 17:57:45 +0100

    edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'gossips#index'
-  
-  get 'gossips/:id', to: 'gossips#show'
+
+  get 'gossips/:id', to: 'gossips#show', as: 'gossip'
   get 'welcomes/:first_name', to: 'welcomes#index'
   get '/team', to: 'static_pages#team'
   get '/contact', to: 'static_pages#contact'
