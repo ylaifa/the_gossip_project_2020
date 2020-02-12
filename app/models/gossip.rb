d0f2621fb6bad95ef856dad4ac01bb49c7f53d64
Yousri <laifayousri@gmail.com>
2020-02-12 11:19:22 +0100

    add validations to gossip model

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -1,4 +1,7 @@
 class Gossip < ApplicationRecord
+    validates :title, length: { in: 3..14}, presence: true
+    validates :content, presence: true
+    
     belongs_to :user
     has_many :comments
     has_many :gossip_tags
