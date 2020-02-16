51850d17b2c60ddb32e8b4422c72dc0b123ea013
Yousri <laifayousri@gmail.com>
2020-02-16 16:21:36 +0100

    add tag to gossips_controller

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -13,6 +13,7 @@ class GossipsController < ApplicationController
 
   def create 
     @gossip = Gossip.new(gossip_params.merge(user: User.first)) 
+    @gossip.tags << Tag.find(params[:tags_id])
     if @gossip.save
       flash[:notice] = 'New gossip created !'
       redirect_to root_path
@@ -28,6 +29,7 @@ class GossipsController < ApplicationController
 
   def update
     @gossip = Gossip.find(params[:id])
+    @gossip.tags << Tag.find(params[:tags_id])
     if @gossip.update(gossip_params.merge(user: User.first))
       flash[:notice] = 'gossip updated !'
       redirect_to @gossip
@@ -46,6 +48,6 @@ class GossipsController < ApplicationController
 
   private
     def gossip_params
-      params.require(:gossip).permit(:title, :content)
+      params.require(:gossip).permit(:title, :content, :tag_ids => [])
     end
 end
