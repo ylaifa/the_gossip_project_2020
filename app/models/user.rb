0e00bf664902a2545065f0f1e2eb7db1cb36daf7
Yousri <laifayousri@gmail.com>
2020-02-17 11:17:28 +0100

    add bcrypt and validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :password, presence: true, length: { minimum: 6 }
+
     belongs_to :city
     has_many :gossips
     has_many :comments
